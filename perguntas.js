182dea41911f371aaf8da0c0f5abadf7a43f034e
kevyncbjr <00001105897722sp@al.educacao.sp.gov.br>
2024-11-27 08:58:32 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Música',
+    'Primeiro romântico da história?',
+    'o priemeiro romântico em obras líricas é DJAVAN'
+)
+
+criaCartao(
+    'Motocicleta',
+    'Melhor moto para uso casual?',
+    'a melhor moto para esse uso, é a XRE 300, ideal para estrada e cidade'
+)
+
+criaCartao(
+    'Música',
+    'Qual o ano lírico do rap nacional?',
+    'se considera 2018 como o ano lírico, por esse ano ter albúns como "Castelos e Rúinas- Bk" e "O menino queria ser Deus- Djonga"'
+)
+
+criaCartao(
+    'Esporte',
+    'Time mais freguês do corinthians?',
+    'o time mais freguês é o São Paulo, com 26 vitórias a menos'
+)
